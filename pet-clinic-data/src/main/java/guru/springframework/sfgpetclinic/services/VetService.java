0f6840ade6595e2c575fcd8942a82472f7049993
Csaba Vadász <77699103+Csaba79-coder@users.noreply.github.com>
2021-10-11 16:59:32 +0200

    adding additional txt files with codes before refactor - fyi - refactoring to common crud intreface. closes #11

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VetService.java
@@ -2,13 +2,7 @@ package guru.springframework.sfgpetclinic.services;
 
 import guru.springframework.sfgpetclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-
-    Vet findById(Long id);
+public interface VetService extends CrudService<Vet, Long> {
 
     Vet saveOwner(Vet vet);
-
-    Set<Vet> findAll();
 }
