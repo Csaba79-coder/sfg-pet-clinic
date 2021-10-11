4de577a83d2c271f222f38171f1e5377bd853d2e
Csaba Vadász <77699103+Csaba79-coder@users.noreply.github.com>
2021-10-12 01:40:28 +0200

    adding vets page and controller. closes #7

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
@@ -0,0 +1,15 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+    @RequestMapping({"vets/index", "/vets/index", "/vets/index.html"})
+    public String listVets() {
+
+        // reason: this is index.html in vets folder under templates!!! this is the most important thing to know!
+        return "vets/index";
+    }
+}
