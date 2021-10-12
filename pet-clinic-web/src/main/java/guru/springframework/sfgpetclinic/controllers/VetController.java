7e696f57eab8510bddc90bfaf19f235cb111919a
Csaba Vadász <77699103+Csaba79-coder@users.noreply.github.com>
2021-10-12 02:01:38 +0200

    create owner index and controller. closes #13

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class VetController {
 
-    @RequestMapping({"vets/index", "/vets/index", "/vets/index.html"})
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
     public String listVets() {
 
         // reason: this is index.html in vets folder under templates!!! this is the most important thing to know!
